0ee1c5b77fb6b46e3a51ee3d9804833436135a67
Edwin Ortiz <edarcode@gmail.com>
2024-10-02 01:06:44 -0500

    maquetando form de registro

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,3 +1,21 @@
+import Btn from "../../components/buttons/Btn/Btn";
+import InputPassword from "../../components/inputs/InputPassword/InputPassword";
+import InputText from "../../components/inputs/InputText/InputText";
+import LinkTo from "../../components/links/LinkTo/LinkTo";
+import { HOME } from "../../router/children";
+import css from "./css.module.css";
+
 export default function Register() {
-  return <div>Register</div>;
+  return (
+    <div className={css.register}>
+      <form className={css.form}>
+        <InputText placeholder="Correo" />
+        <InputPassword placeholder="Contraseña" />
+        <span className={css.wrapper_register}>
+          ¿Ya tiene cuenta?<LinkTo to={HOME.to}>Iniciar sesión</LinkTo>
+        </span>
+        <Btn>Registrarse</Btn>
+      </form>
+    </div>
+  );
 }

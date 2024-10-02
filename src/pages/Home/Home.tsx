fed438844f111ff3bcc209aa82b42362ca5760f1
Edwin Ortiz <edarcode@gmail.com>
2024-10-02 01:02:27 -0500

    agg vista register

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,8 @@
 import Btn from "../../components/buttons/Btn/Btn";
 import InputPassword from "../../components/inputs/InputPassword/InputPassword";
 import InputText from "../../components/inputs/InputText/InputText";
+import LinkTo from "../../components/links/LinkTo/LinkTo";
+import { REGISTER } from "../../router/children";
 import css from "./css.module.css";
 
 export default function Home() {
@@ -9,6 +11,9 @@ export default function Home() {
       <form className={css.login}>
         <InputText placeholder="Correo" />
         <InputPassword placeholder="Contraseña" />
+        <span className={css.wrapper_register}>
+          ¿No tiene cuenta?<LinkTo to={REGISTER.to}>Registrarse</LinkTo>
+        </span>
         <Btn>Iniciar sesión</Btn>
       </form>
     </div>
--- src/router/children.tsx
+++ src/router/children.tsx
@@ -1,4 +1,5 @@
 import Home from "../pages/Home/Home";
+import Register from "../pages/Register/Register";
 
 export const HOME = {
   id: crypto.randomUUID(),
@@ -6,4 +7,12 @@ export const HOME = {
   to: "/",
   display: "Inicio",
   element: <Home />,
+};
+
+export const REGISTER = {
+  id: crypto.randomUUID(),
+  path: "registro",
+  to: "/registro",
+  display: "Registro",
+  element: <Register />,
 };
